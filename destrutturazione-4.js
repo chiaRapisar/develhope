9c18c87a0f040c9b365038a96fa987f788a85bc1
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-11-22 11:28:50 +0100

    destr4

---
+++ destrutturazione-4.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+
+const change=[a,b];
+const[B,A]=change;
+console.log(B);
+console.log(A);
+
+// non sono sicura di aver capito la consegna
+// Date due variabili a e b, scambia i loro valori usando la destrutturazione.
