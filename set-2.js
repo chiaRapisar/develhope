2c778f3b5af230e8c2361bbe03ffbe50344c978d
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-11-27 12:32:40 +0100

    ContoAllaRovescia

---
+++ set-2.js
@@ -0,0 +1,14 @@
+let tempoRimanente =10;
+console.log("Conto alla rovescia in corso ...");
+function contoAllaRovescia() {
+    const tempo = setInterval(() => {
+        tempoRimanente--;
+        console.log(`Rimane ${tempoRimanente}`)
+    }, 1000);
+
+setTimeout(() => {
+    clearInterval(tempo);
+    console.log("Conto alla rovescia terminato!")
+}, 10000);}
+
+contoAllaRovescia();
