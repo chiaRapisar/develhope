693133a2aa62459cd44e12bd0656cda150c1525c
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-12-01 12:01:07 +0100

    Gestione Err

--- gestioneErr.js
+++ gestioneErr.js
@@ -1,21 +1,45 @@
 function performOperation(a, b, callback) {
-    if (typeof a != "number" || typeof b != "number") {
-        throw new Error("Inserisci un numero")
+    const sum = a + b;
+    callback(sum);
+  }
+  
+  function displayResult(result) {
+    if (typeof result ==="number") {
+        console.log(result)
+    } else{
+        console.log("Error")
     }
-    try {
-        let operations = (a * b) + (a + b);
-        callback(null, operations)
-    } catch (error) {
-        callback(error, null)
-    }
+    
+  }
+  
+  performOperation(5, "3", displayResult);
 
-}
 
-function displayResult(error, data) {
-    if (error) {
-        console.error(error)
-    } else
-        console.log(`Il risultato dell'operazione è ${data}`);
-}
 
-performOperation(5, 3, displayResult)
+
+
+
+
+
+//CON IL PROF
+// function performOperation(a, b, callback) {
+//     if (typeof a != "number" || typeof b != "number") {
+//         throw new Error("Inserisci un numero")
+//     }
+//     try {
+//         let operations = (a * b) + (a + b);
+//         callback(null, operations)
+//     } catch (error) {
+//         callback(error, null)
+//     }
+
+// }
+
+// function displayResult(error, data) {
+//     if (error) {
+//         console.error(error)
+//     } else
+//         console.log(`Il risultato dell'operazione è ${data}`);
+// }
+
+// performOperation(5, 3, displayResult)
