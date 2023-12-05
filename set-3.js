fbbc5c290560c774ee5dc0e6bc709b09201d56a8
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-12-05 10:51:33 +0100

    5-12

--- set-3.js
+++ set-3.js
@@ -1,5 +1,8 @@
 function mostraPromemoria() {
     console.log("Promemoria: È ora di fare una pausa!");
   }
+ function Promemoria() {
   console.log("Il promemoria è in fase d'impostazione");
+ } 
   setTimeout(mostraPromemoria, 5000);
+  setTimeout(Promemoria, 2000);
