2c92ac85bc711b5c454258651de5d8555ceed67c
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-12-05 10:30:54 +0100

    Promise3

--- promise3.js
+++ promise3.js
@@ -1,38 +1,16 @@
-// const myPromise = new Promise ((resolve, reject)=>{
-//     setTimeout(() => {
-//         // reject(new Error('promise fallita'))
-//         resolve('promise risolta')
-//     }, 2000);
-// })
-
-// myPromise
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
-
-//Altro esempio
-const number = 5
+const number = 10
 const myPromise = new Promise ((resolve, reject)=>{
     if(number >=5){
     setTimeout(() => {
-        resolve('prima promise risolta')
+        resolve('Promise risolta')
     }, 2000);
 } else {
     setTimeout(() => {
-        reject(new Error('promise fallita'))
+        reject(new Error('Promise fallita'))
     }, 2000);
 }
 })
 
-const secondPromise = new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        resolve('seconda promise risolta')
-    }, 1000);
-})
-
 myPromise
-.then(data => {
-    console.log(data);
-    return secondPromise
-})
-.then(result => console.log(result))
+.then(data => console.log(data))
 .catch(err => console.log(err))
