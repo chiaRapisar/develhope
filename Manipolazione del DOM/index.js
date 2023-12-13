f98eb383de907f164995765693662cbcb36bf682
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2023-12-13 15:07:23 +0100

    todo

--- Manipolazione del DOM/index.js
+++ Manipolazione del DOM/index.js
@@ -1,3 +1,19 @@
-const addProduct = () => {
-  // ...
-};
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+function addProduct() {
+  if(input.value !=''){
+    const li =document.createElement('li')
+    ul.appendChild(li)
+    li.innerHTML = `${input.value} <input type="checkbox" name="check" id="check">`
+    input.value=''
+  } else {
+    console.log('inserisci un dato')
+  }
+  
+}
+
+btn.addEventListener('click', addProduct )
+
+
