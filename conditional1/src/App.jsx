f4325fdcf557916cc7e12c0134c755b8e7e51870
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-01-18 17:58:43 +0100

    conditional2

--- conditional1/src/App.jsx
+++ conditional1/src/App.jsx
@@ -3,7 +3,7 @@ import { Welcome } from "./Welcome";
 export function App() {
     return (
         <div>
-            <Welcome name="John" age={60}/>
+            <Welcome name="John" age={60} />
         </div>
     )
     
--- conditional1/src/Welcome.jsx
+++ conditional1/src/Welcome.jsx
@@ -1,4 +1,5 @@
 import { Age } from "./Age";
+import { AgeAlfio } from "./AgeAlfio";
 import { Hello } from "./Hello";
 
 export function Welcome({name, age}) {
@@ -9,6 +10,9 @@ export function Welcome({name, age}) {
         {!!age && <Age age ={age + ". " +"Age esiste"} />}
         {age >=18 && age <=65 && <Age age={age + ". " + "Maggiore di 18, minore di 65"}/>}
         {age >=18 && age <=65 && name==="John" && <Age age={age + ". " +"John è maggiore di 18 e minore di 65"}/>}
+        <hr />
+        //Esercizio Conditional 2
+        {age>=18 ? <Age age={age}/> : <p>You are very young!</p>}
         </div>
     )
     
