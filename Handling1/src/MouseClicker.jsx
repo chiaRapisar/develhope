9363fed421febecaf1bc4ffa92315497fe6d4069
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-01-19 19:24:47 +0100

    Handling2

--- Handling1/src/MouseClicker.jsx
+++ Handling1/src/MouseClicker.jsx
@@ -1,12 +1,21 @@
 export function MouseClicker() {
-    function handleButtonClick(event) {
-        console.log(event.target.name)
-        
-    }
-    return (
-        <button name="one" onClick={handleButtonClick}>
-            Cliccamiii
-            </button>
-    )
-    
+  function handleButtonClick(event) {
+    console.log(event.target.name);
+  }
+  //Esercizio 2 con secondo button
+  function printSrc(event) {
+    console.log(event.target.src);
+  }
+  return (
+    <div>
+      <button name="one" onClick={handleButtonClick}>
+        Cliccami
+      </button>
+      <br/>
+      <br />
+      <button onClick={printSrc}>
+        <img src="https://cdn.icon-icons.com/icons2/1674/PNG/64/image_111003.png" />
+      </button>
+    </div>
+  );
 }
