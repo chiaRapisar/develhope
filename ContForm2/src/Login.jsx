ee1732afcf9742a80f079679c6fb347b9a3b82a9
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-01-23 19:50:45 +0100

    ContForm3

--- ContForm2/src/Login.jsx
+++ ContForm2/src/Login.jsx
@@ -1,38 +1,58 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function Login() {
-    const [data, setData] = useState({
-        username:'',
-        password:'',
-        checked: false,
-    })
+export function Login({onLogin}) {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    checked: false,
+  });
 
-    function handleDataInput(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-        const checked = event.target.checked;
-        const type = event.target.type;
+  function handleDataInput(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
 
-        setData((d)=>{
-            return {
-                ...d,
-                [name]: type ==="checkbox" ? checked : value
-            }
-        })
-        
-    }
-    return(
-        <div>
-            <h3>Name</h3>
-        <input name="username" type="text" value={data.username} onChange={handleDataInput}/>
+    setData((d) => {
+      return {
+        ...d,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+  //Es 3
+  function onLogin(data) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <h3>Name</h3>
+      <input
+        name="username"
+        type="text"
+        value={data.username}
+        onChange={handleDataInput}
+      />
+      <br />
+      <h3>Password</h3>
+      <input
+        name="password"
+        type="password"
+        value={data.password}
+        onChange={handleDataInput}
+      />
+      <br />
+      <input
+        name="checked"
+        type="checkbox"
+        value={data.checked}
+        onChange={handleDataInput}
+      />
         <br />
-        <h3>Password</h3>
-        <input name="password" type="password" value={data.password} onChange={handleDataInput} />
-        <br />
-        <input name="checked" type="checkbox" value={data.checked} onChange={handleDataInput} />
-
-        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-        </div>
-    )
-    
+      <button disabled={!data.username || !data.password} onClick={()=> onLogin(data)}>
+        Login
+      </button>
+    </div>
+  );
 }
