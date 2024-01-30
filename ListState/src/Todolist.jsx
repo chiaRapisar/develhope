721a11aa6bb6ba51cffc49046426733c1569af43
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-01-30 20:47:34 +0100

    Non chiari. Non inviati

--- ListState/src/Todolist.jsx
+++ ListState/src/Todolist.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export function TodoList() {
-  const [todos, setTodos] = useState([]); //non ho chiaro il perchè
+  const [todos, setTodos] = useState([]); 
   const [newTodo, setNewTodo] = useState("");
 
   function handleInput(e) {
@@ -12,18 +12,30 @@ export function TodoList() {
     setTodos((e) => [...e, newTodo]);
     setNewTodo('');
   }
-  console.log(todos);
-  console.log(newTodo);
+  function handleReset() {
+    setTodos([])
+  }
+  
+  function removeInput(i) {
+    setTodos((arr)=> arr.filter((tdo, index)=> index !=i))
+    
+  }
+  
   return (
     <div>
         <input name="todos" onChange={handleInput} value={newTodo} type="text" />
          <button onClick={handleBtn}>ADD</button>
+         <button onClick={handleReset}>RESET</button>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>{todo} <button onClick={() => removeInput(index)}>RIMUOVI</button></li>
+          
         ))}
       </ul>
       
     </div>
   );
-}
+} 
+//Non mi è chiaro
+
+
