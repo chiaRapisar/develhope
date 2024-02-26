f4427ee6f02bce53d7e82157784fe9133fd6511c
Chiara <148362799+chiaRapisar@users.noreply.github.com>
2024-02-26 23:15:41 +0100

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,25 @@
+-- TABLE
+CREATE TABLE Books (
+book_id INTEGER PRIMARY KEY,
+title TEXT VARCHAR (100),
+author TEXT VARCHAR (100),
+genre TEXT VARCHAR (100),
+published_year INT,
+isbn VARCHAR (20),
+price  DECIMAL (10, 2),
+rating DECIMAL (3,2),
+stock_count  INT
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE dog (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+gender TEXT VARCHAR (1)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
