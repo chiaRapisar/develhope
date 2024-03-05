e92733d728cc15ebdcfa540537a6d3e8cb9cac75
chiaRapisar <148362799+chiaRapisar@users.noreply.github.com>
2024-03-05 15:15:22 +0100

    primo eser

--- Node/Ese1.js
+++ Node/Ese1.js
@@ -1,5 +1,10 @@
-const a = 5;
-const b = 6;
-console.log(a+b)
+// const a = 5;
+// const b = 6;
+// console.log(a+b)
+
+function sum(a,b) {
+    return a+b;
+}
+console.log(sum(5,6));
 
 // Da terminale: node Ese1.js 
